7fbed0a20d0051e98aff1296715de41193213c20
Balázs Hunor <hunorgaming000@gmail.com>
2024-02-13 14:59:05 +0100

    Slider

--- src/main/java/com/example/keringo/HelloController.java
+++ src/main/java/com/example/keringo/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.keringo;
 
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Slider slSpeed;
+    public Label lbSpeed;
+    public Slider slRadius;
+    public Label lbRadius;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        lbSpeed.textProperty().bind(Bindings.format("Speed: %.0f", slSpeed.valueProperty()));
+        lbRadius.textProperty().bind(Bindings.format("Radius: %.0f", slRadius.valueProperty()));
+    }
+
+    public void onClickStart() {
     }
 }
